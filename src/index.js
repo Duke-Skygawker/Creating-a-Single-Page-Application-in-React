f9ace284ba7aed0e921d70415616f37024d4113e
Duke-Skygawker <bart3kk990@gmail.com>
2023-01-05 04:04:31 +0100

    added some bootstrap styling to all components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import List from "./components/List"
+import List from "./components/List";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-    return (
-        <div>
-            <h1>Rick and Morty</h1>
-            <List/>
-        </div>
-    )
+  return (
+    <div className="container">
+      <nav className="navbar navbar-light bg-dark">
+        <span className="navbar-brand h1 text-light">Rick and Morty</span>
+        {/* for some reason margin refuses to work on the nav */}
+      </nav>
+      <List />
+    </div>
+  );
 }
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
-root.render(<App/>)
+root.render(<App />);
